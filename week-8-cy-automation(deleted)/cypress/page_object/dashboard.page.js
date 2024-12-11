e3d68dfd7afd3bec6967a66104c9fd3e0e614d42
IG1989 <ivan.igritsuk@gmail.com>
2024-12-10 17:55:33 -0800

    Delete week-8-cy-automation(deleted)/cypress/page_object directory

--- week-8-cy-automation(deleted)/cypress/page_object/dashboard.page.js
+++
@@ -1,7 +0,0 @@
-class DashboardPage {
-    get fullNameInput() {return cy.get("a h6")};
-    get roleType() {return cy.get("a p")};
-    get personIcon() {return cy.get('.css-1x4nai .MuiButtonBase-root MuiIconButton-root.MuiIconButton-sizeMedium css-10v8o86"]')};
-  }
-  
-  export default new DashboardPage();
