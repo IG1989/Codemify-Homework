14f2cc69b19a92da5464a7023da3ac3c1d9d50cf
IG1989 <ivan.igritsuk@gmail.com>
2024-12-12 21:09:24 -0800

    Done

--- week-8-cy-automation/cypress/page_object/dashboard.page.js
+++ week-8-cy-automation/cypress/page_object/dashboard.page.js
@@ -1,7 +1,8 @@
 class DashboardPage {
-  
+
   get fullNameInput() { return cy.get("a h6"); }
   get roleType() { return cy.get("a p"); }
+  get personIcon() { return cy.get('button [data-testid="PersonIcon"]'); }
   get roleLabel() { return cy.get('a p') };
   get fullNameLabel() { return cy.get('h6') };
   get userIconBtn() { return cy.get('[type="button"]').eq(0); }
